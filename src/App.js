4882c357b9ffd5246fb1f6305ceb1da530d19f69
Adarsh Malviya <adarshmalviya1199@gmail.com>
2023-02-08 09:54:32 +0530

    Feature section created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Intro from './components/Intro'
 import Features from './components/Features'
 
 const Container = styled.div`
 height: 100vh;
 overflow: hidden;
+position: relative;
 `
-const IntroShape = styled.div`
-  width: 100%;
+const shape = css`
+ width: 100%;
   height: 100%;
   position: absolute;
   top: 0;
   left: 0;
   z-index: -1;
+`
+
+const IntroShape = styled.div`
+ ${shape}
   clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
  background-color: crimson;
+`;
+
+const FeatureShape = styled.div`
+ ${shape}
+  clip-path: polygon(0 0%,55% 0%, 35% 100%, 0% 100%);
+ background-color: pink;
 `;
 
 function App() {
@@ -29,6 +40,7 @@ function App() {
             </Container>
             <Container>
                 <Features />
+                <FeatureShape />
             </Container>
         </>
     )
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,29 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
 import App from '../img/app.png'
+import AnimatedShapes from './AnimatedShapes'
 
 const Container = styled.div` 
 display: flex;
 `
+
+//--left side --
 const Left = styled.div` 
 width:50%;
 
 `
 const Image = styled.img` 
 width: 80%;
+margin-top: 20px;
 `
+// --right side --
 const Right = styled.div` 
 width: 50%;
+display: flex;
+flex-direction: column;
+justify-content: center;
 `
+const Title = styled.span`
+font-size: 70px;
+`
+const SubTitle = styled.span`
+font-size: 24px;
+color: #333;
+font-style: italic;
+margin-top: 20px;
+`
+const Desc = styled.p`
+font-size: 20px;
+color: #777;
+margin-top: 30px;
+`
+const Button = styled.button`
+width: 150px;
+padding: 10px 15px;
+background-color: darkblue;
+color: white;
+border-radius: 20px;
+margin-top: 20px;
+cursor: pointer;
+;
+`
+
 const Features = () => {
     return (
         <Container>
-            <Right>
-                <Image src={App} />
-            </Right>
             <Left>
-
+                <Image src={App} />
             </Left>
+            <Right>
+                <Title>
+                    <b>Good </b> Design <br />
+                    <b>Good </b> Business
+                </Title>
+                <SubTitle>We know that good design means good business.</SubTitle>
+                <Desc>
+                    We help our clients succeed by creating brand identities, digital
+                    experiences, and print materials that communicate clearly, achieve
+                    marketing goals, and look fantastic.
+                </Desc>
+                <Desc>
+                    We care your business and guarantee you to achieve marketing goals.
+                </Desc>
+                <Button>Learn More</Button>
+            </Right>
+            <AnimatedShapes/>
         </Container>
     )
 }
